e0ea00398044d8fa82e2eb58792f03b3a2f07d5d
sean <sean@gomicorp.com>
2021-09-06 00:24:43 +0900

    volumeCredits 변수 제거하기 - 2. 문장 슬라이드로 선언문을 반복문 앞으로 이동

--- statement.js
+++ statement.js
@@ -3,7 +3,6 @@ import {plays} from './plays.js';
 
 const statement = (invoice, plays) => {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
   function usd(aNumber) {
@@ -53,6 +52,7 @@ const statement = (invoice, plays) => {
     totalAmount += amountFor(perf);
   }
 
+  let volumeCredits = 0;
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
   }
