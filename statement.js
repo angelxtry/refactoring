60b60c67592385cd7847a4bbc3c39f6e7ca31d8e
sean <sean@gomicorp.com>
2021-09-05 23:19:24 +0900

    switch 구문을 별도의 함수로 분리

--- statement.js
+++ statement.js
@@ -11,27 +11,32 @@ const statement = (invoice, plays) => {
     minimumFractionDigits: 2
   }).format;
 
-  for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+  function amountFor(play, perf) {
     let thisAmount = 0;
 
     switch (play.type) {
-    case "tragedy":
-      thisAmount = 40000;
-      if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30);
-      }
-      break;
-    case "comedy":
-      thisAmount = 30000;
-      if (perf.audience > 20) {
-        thisAmount += 10000 + 500 * (perf.audience - 20);
-      }
-      thisAmount += 300 * perf.audience;
-      break;
-    default:
-      throw new Error(`알 수 없는 장르: ${play.type}`);
+      case "tragedy":
+        thisAmount = 40000;
+        if (perf.audience > 30) {
+          thisAmount += 1000 * (perf.audience - 30);
+        }
+        break;
+      case "comedy":
+        thisAmount = 30000;
+        if (perf.audience > 20) {
+          thisAmount += 10000 + 500 * (perf.audience - 20);
+        }
+        thisAmount += 300 * perf.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${play.type}`);
     }
+    return thisAmount;
+  }
+
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID];
+    let thisAmount = amountFor(play, perf);
 
     volumeCredits += Math.max(perf.audience - 30, 0);
 
