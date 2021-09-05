6d991d4be7c57ca466fc0ebf13434ab61e5d0742
sean <sean@gomicorp.com>
2021-09-06 00:21:12 +0900

    volumeCredits 변수 제거하기 - 1. 반복문 쪼개기

--- statement.js
+++ statement.js
@@ -49,10 +49,12 @@ const statement = (invoice, plays) => {
   }
 
   for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-
     result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
     totalAmount += amountFor(perf);
+  }
+
+  for (let perf of invoice.performances) {
+    volumeCredits += volumeCreditsFor(perf);
   }
 
   result += `총액: ${usd(totalAmount)}\n`;
